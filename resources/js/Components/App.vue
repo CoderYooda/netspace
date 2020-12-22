2dd857a3d2db070276422960222415b4b5946251
CoderYooda <coderyooda@gmail.com>
2020-12-22 10:59:19 +0300

    awd

--- resources/js/Components/App.vue
+++ resources/js/Components/App.vue
@@ -1,12 +1,36 @@
 <template>
-    <div class="lk__offset">
-    <component :is="layout" />
-<!--        <login/>-->
+    <div>
+        <div class="lk__header">
+            <div class="container">
+                <div class="row">
+                    <a href="/" class="lk__header-logo" title="NetSpace">
+                        <img src="/images/logo/logo.png" alt="NetSpace" class="lk__header-logo-img">
+                    </a>
+                    <div class="row lk__contacts">
+                        <div class="lk__contacts-item">
+                            <div class="lk__contacts-label">Круглосуточно</div>
+                            <a href="tel:+71234567891" class="lk__contacts-link link_phone">+7 47244 250 911</a>
+                        </div>
+                        <div class="lk__contacts-item">
+                            <div class="lk__contacts-label">Напишите нам</div>
+                            <a href="mailto:" class="lk__contacts-link">net_space@mail.ru</a>
+                        </div>
+                        <button class="lk__logout" title="Выйти из личного кабинета"><span class="btn_text_hide">Выйти</span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lk__offset">
+            <component :is="layout" />
+            <!--        <login/>-->
+        </div>
     </div>
+
 </template>
 
 <script>
     import MainLayout from './layouts/MainLayout'
+    import ErrorLayout from './layouts/ErrorLayout'
     import Login from './auth/Login'
     export default {
         data: ()=> {
@@ -27,7 +51,8 @@
         },
         components: {
             Login,
-            MainLayout
+            MainLayout,
+            ErrorLayout
         }
     }
 </script>
---
+++ resources/js/Components/layouts/ErrorLayout.vue
@@ -0,0 +1,92 @@
+<template>
+
+        <div class="container">
+            <div class="row lk__row">
+
+                <div class="lk__sidebar-wrap">
+
+                    <button class="btn lk__payment-btn lk__sidebar-btn-sber sber_icon">
+                        Оплатить<span class="btn_text_hide">&nbsp;через СБЕР</span>
+                    </button>
+
+                    <div class="lk__sidebar">
+                        <div class="lk__username">
+                            <div class="lk__surname">Яковенко</div>
+                            Алина Олеговна
+                        </div>
+
+                        <div class="lk__info">
+                            <div class="lk__info-item">
+                                <div class="lk__info-name">Номер счета</div>
+                                <div class="lk__info-value">7768885777588</div>
+                            </div>
+
+                            <div class="lk__info-item">
+                                <div class="lk__info-name">Договор</div>
+                                <div class="lk__info-value">ВМ636377737</div>
+                            </div>
+
+                            <div class="lk__info-item">
+                                <div class="lk__info-name">Статус</div>
+                                <div class="lk__info-value lk__info-status active">Активен</div>
+                            </div>
+
+                            <div class="lk__info-item">
+                                <div class="lk__info-name">Тариф</div>
+                                <div class="lk__info-value">Свой 1000 руб.</div>
+                            </div>
+
+                            <div class="lk__info-item">
+                                <div class="lk__info-name">Дата активации</div>
+                                <div class="lk__info-value">11.12.2020</div>
+                            </div>
+
+                            <div class="lk__info-item lk__sidebar-balance">
+                                <div class="lk__info-name">Баланс</div>
+                                <div class="lk__info-value"><b>840 руб.</b></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="lk__sidebar-footer">
+                        <button class="btn lk__payment-btn lk__sidebar-btn">Помощь</button>
+                        <button class="btn lk__payment-btn lk__sidebar-btn lk__sidebar-btn-offset">Вызов мастера</button>
+                    </div>
+                </div>
+
+                <div class="lk__content">
+
+                    <div class="lk__tabs-wrap">
+                        <!--                        Эта обертка для адаптива-->
+
+                        <!--                        Если это компьютер .active добавлять к .lk__tab при размере экрана меньше 655px .active добавлять .tab-->
+                        <Menu/>
+                    </div>
+                    <div class="lk__tab-content-wrap">
+                        <router-view />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+</template>
+
+<script>
+    import Menu from '../views/Menu'
+    export default {
+        data: ()=> {
+            return {
+                isDark: false,
+            }
+        },
+        created(){
+        },
+
+        methods: {
+
+        },
+        components: {
+            Menu
+        }
+    }
+</script>
