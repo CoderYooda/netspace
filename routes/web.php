dc00ca7a87f4dfff87ce715a0489e7fbbe2f2bb4
CoderYooda <CoderYooda@gmail.com>
2020-12-27 22:51:49 +0300

    awd

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/home', 'base')->name('lk');
-Route::view('/login', 'login')->name('login');
-
 Route::view('/{any?}', 'index')->name('index');
 
 Route::view('/home', 'base')->name('lk');
